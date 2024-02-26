4f9669543c15801364053c9424fae8116ae18c11
haris700 <harisvkvnr@gmil.com>
2024-02-26 21:29:45 +0530

    small changes

--- src/common.ts
+++ src/common.ts
@@ -2,6 +2,8 @@ import { PrismaClient } from "@prisma/client";
 
 const userRepo = new PrismaClient().user;
 
+console.log(userRepo);
+
 export const findUserByEmail = async (email: string) => {
   return await userRepo.findUnique({
     where: {
